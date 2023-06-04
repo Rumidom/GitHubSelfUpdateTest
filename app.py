4fdeeabb611a51d14c86c75d276c194da999bee8
Rumidom <roberto.canuto@hotmail.com>
2023-06-04 10:41:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
 from flask import Flask, render_template, request, redirect,Response
 import git
+import os
 
 app = Flask(__name__)
+repo = git.Repo(os.path.dirname(os.path.abspath(__file__)))
 
 @app.route('/')
 def index():
+	repo.remotes.origin.fetch()
 	current = repo.head.commit
-	repo.remotes.origin.pull()
-	if current != repo.head.commit:
-    	print("new update available")
+	latest_commit = repo.remotes.origin.refs['HEAD'].commit
+	if current != latest_commit:
+		print("new update available")
 
-    return render_template('index.html')
+	return render_template('index.html')
 
 @app.route('/update')
 def update():
-	repo = git.Repo('/')
-	repo.remotes.origin.pull()
-    return "This is the update page. updated"
+	
+	repo.remotes.origin.pull(force=True)
+	return "This is the update page. Updated"
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+	app.run(host='0.0.0.0', port=80)
